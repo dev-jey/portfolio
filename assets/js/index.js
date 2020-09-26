21f3f3bcf71da70632618b35cd0210a16cd86a28
dev-jey <savalijey@gmail.com>
2020-09-26 06:05:42 +0300

    Add navbar

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+
+let loader_holder = document.getElementsByClassName("loader-holder")[0];
+
+function hideLoader() {
+    loader_holder.style.visibility = "hidden";
+    loader_holder.style.opacity=0
+}
