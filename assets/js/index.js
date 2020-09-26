bf022e0512314207db3fb0f6978a8a5bbcc17463
dev-jey <savalijey@gmail.com>
2020-09-26 08:00:39 +0300

    Add about content

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,18 @@
 
 let loader_holder = document.getElementsByClassName("loader-holder")[0];
+var myNav = document.getElementById('navbar');
+var myLogo = document.getElementById('logo-right');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 70 || document.documentElement.scrollTop >= 70 ) {
+        myNav.style.background="#fff";
+        myNav.style.boxShadow="0px 0px 10px silver";
+    } 
+    else {
+        myNav.style.background="none"
+        myNav.style.boxShadow="none";
+    }
+};
 
 function hideLoader() {
     loader_holder.style.visibility = "hidden";
