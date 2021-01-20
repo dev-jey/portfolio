3c0837d8086f530e0ae7da20e1983046c77bd58f
dev-jey <savalijey@gmail.com>
2021-01-20 20:23:39 +0300

    Work in progress

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,17 @@
 
-let loader_holder = document.getElementsByClassName("loader-holder")[0];
-var myNav = document.getElementById('navbar');
+var myNav = document.getElementById('home');
+
+function scrollNav() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        myNav.classList.add("whit-bg");
+    } else {
+        myNav.classList.remove("whit-bg");
+    }
+}
+
 var myLogo = document.getElementById('logo-right');
-window.onscroll = function () {
-    "use strict";
+
+function scrollSkills() {
     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
         const progress = document.querySelectorAll('.progress-done');
         progress.forEach(p => {
@@ -13,7 +21,7 @@ window.onscroll = function () {
             }, 500)
         });
     }
-};
+}
 
 document.addEventListener("DOMContentLoaded", function () {
     var lazyloadImages;
@@ -64,12 +72,61 @@ document.addEventListener("DOMContentLoaded", function () {
         window.addEventListener("orientationChange", lazyload);
     }
 })
-function hideLoader() {
-    loader_holder.style.visibility = "hidden";
-    loader_holder.style.opacity = 0
+
+
+
+
+
+
+
+
+var modal = document.getElementById("modal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("card1").addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.add("show");
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.classList.remove("show");
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.classList.remove("show");
+    }
 }
 
 
 
 
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+    scrollNav();
+    scrollSkills();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
