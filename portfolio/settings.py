64417e9c602aadc98ab61f434b2f47964dc343e2
dev-jey <savalijey@gmail.com>
2021-01-22 18:25:23 +0300

    Fix tiny mce editor

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -145,8 +145,8 @@ STATICFILES_DIRS = (
 )
 
 
-TINYMCE_JS_URL = os.path.join(STATIC_URL, "assets/js/tiny_mce/tinymce.min.js")
-TINYMCE_JS_ROOT = os.path.join(STATIC_URL, "assets/js/tiny_mce")
+TINYMCE_JS_URL = os.path.join(STATIC_URL, "tiny_mce/tinymce.min.js")
+TINYMCE_JS_ROOT = os.path.join(STATIC_URL, "tiny_mce")
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
