4c2fbbe1504482d6fc80cca5214ed04461040e1f
dev-jey <savalijey@gmail.com>
2021-01-24 14:14:54 +0300

    Fix url redirect issue

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -35,11 +35,8 @@ if os.environ.get('CURRENT_ENV') == 'development':
         os.path.join(BASE_DIR, "static"),
     )
 else:
-    # STATIC_ROOT = os.path.join(BASE_DIR,  'static')
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, "static"),
-    )
-    DEBUG = True
+    STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+    DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '104.236.16.20', 'localhost']
 
