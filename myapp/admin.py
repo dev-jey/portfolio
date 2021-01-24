e6c6ddab123e4aec60b5f2d375320ebaf9e4cb6a
dev-jey <savalijey@gmail.com>
2021-01-24 03:05:52 +0300

    Fix all pages content

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from myapp.models import Client, Skill, Specialty, Project
+from myapp.models import Client, Skill, Specialty, Project, ProjectTechnology
 
 admin.site.register(Client)
 admin.site.register(Skill)
 admin.site.register(Specialty)
 admin.site.register(Project)
+admin.site.register(ProjectTechnology)
 
 admin.site.site_header = "DevJey Portfolio"
 admin.site.site_title = "DevJey Portfolio"
