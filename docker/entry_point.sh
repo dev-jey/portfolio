48a980e647d06f7854ce740168665634bdc50919
dev-jey <savalijey@gmail.com>
2021-01-26 02:03:24 +0300

    Add google analytics

---
+++ docker/entry_point.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+source /root/.local/share/virtualenvs/portfolio-*/bin/activate
+
+# pipenv shell
+echo "<<<<<<<<<< Export LANG to the Env>>>>>>>>>>"
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+
+echo "<<<<<<<< Collect Staticfiles>>>>>>>>>"
+python manage.py collectstatic --noinput
+
+
+sleep 10
+echo "<<<<<<<< Database Setup and Migrations Starts >>>>>>>>>"
+# Run database migrations
+python manage.py migrate
+
+
+sleep 5
+echo "<<<<<<< Database Setup and Migrations Complete >>>>>>>>>>"
+echo " "
+
+
+sleep 5
+echo "<<<<<<<<<<<<<<<<<<<< START API >>>>>>>>>>>>>>>>>>>>>>>>"
+# python manage.py runserver 0.0.0.0:8000
+# Start the API with gunicorn
+gunicorn --bind 0.0.0.0:80 portfolio.wsgi --reload --access-logfile '-' --workers 2
